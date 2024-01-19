5a1c659593d3d52213b4d95672ab4851c16033aa
krzysiek1421 <krzysiek1421@gmail.com>
2024-01-19 07:21:33 +0100

    Created interfaces

---
+++ pet-clinic-data/src/main/java/krzysiek1421/springframework/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package krzysiek1421.springframework.petclinic.services;
+
+import krzysiek1421.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
