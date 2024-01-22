f5855d41753335e4cfc3dc5038aa10d78f097b35
krzysiek1421 <krzysiek1421@gmail.com>
2024-01-22 10:44:01 +0100

    impl map based services, closes #4

---
+++ pet-clinic-data/src/main/java/krzysiek1421/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package krzysiek1421.springframework.petclinic.services.map;
+
+import krzysiek1421.springframework.petclinic.model.Owner;
+import krzysiek1421.springframework.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return this.save(owner.getId(), owner);
+    }
+
+}
---
+++ pet-clinic-data/src/main/java/krzysiek1421/springframework/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package krzysiek1421.springframework.petclinic.services.map;
+
+import krzysiek1421.springframework.petclinic.model.Pet;
+import krzysiek1421.springframework.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return this.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
