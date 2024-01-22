ba5f93a48ac8c56c03e007a0bbeec35885175611
krzysiek1421 <krzysiek1421@gmail.com>
2024-01-22 10:22:04 +0100

    refactoring services to common crud interface

--- pet-clinic-data/src/main/java/krzysiek1421/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/krzysiek1421/springframework/petclinic/services/VetService.java
@@ -2,12 +2,5 @@ package krzysiek1421.springframework.petclinic.services;
 
 import krzysiek1421.springframework.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
-
+public interface VetService extends CrudService<Vet, Long>{
 }
